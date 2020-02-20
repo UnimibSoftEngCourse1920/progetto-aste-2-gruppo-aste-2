b4550deaec273f18a669b29c0f86030962c365c8
AuPath <marco.au.grassi98@protonmail.com>
2020-02-20 15:59:16 +0100

    Aggiunta strutura del progetto backend

---
+++ Backend/ProgettoAste/src/main/java/com/GruppoAste2/ProgettoAste/ProgettoAsteApplication.java
@@ -0,0 +1,13 @@
+package com.GruppoAste2.ProgettoAste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoAsteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoAsteApplication.class, args);
+	}
+
+}
---
+++ Backend/ProgettoAste/src/test/java/com/GruppoAste2/ProgettoAste/ProgettoAsteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GruppoAste2.ProgettoAste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoAsteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
