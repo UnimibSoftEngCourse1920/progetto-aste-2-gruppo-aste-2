d6362029ea3d70062dde2d3b091d1d67eacc6471
wailla <f.villa71@campus.unimib.it>
2020-02-21 10:57:27 +0100

    fine utente

---
+++ Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/api/UtenteRegistratoController.java
@@ -0,0 +1,16 @@
+package com.gruppoaste2.progettoaste.api;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/oggetto")
+@RestController
+public class UtenteRegistratoController {
+    @GetMapping
+    public String esempioGet()
+    {
+        return "<h1>Prova ciao hai fatto una get ad localhost:8080/api/oggetto<h1>";
+    }
+}
