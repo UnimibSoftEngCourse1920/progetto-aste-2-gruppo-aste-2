5cd692611e6bf8b93b3bb3278ae11d5e6f1cde1a
wailla <f.villa71@campus.unimib.it>
2020-02-21 11:29:18 +0100

    continuo errore in utenteRegistrtoDAO

--- Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/dao/PostgresUtenteRegistratoDAO.java
+++ Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/dao/PostgresUtenteRegistratoDAO.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-@Repository("postgres")
+@Repository("")
 public class PostgresUtenteRegistratoDAO implements UtenteRegistratoDAO{
 
     @Override
