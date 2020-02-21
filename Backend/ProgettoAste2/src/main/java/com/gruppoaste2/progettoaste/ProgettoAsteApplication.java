999d9942b2f1a9c50082e12d4e92ea5f04548ad6
AuPath <marco.au.grassi98@protonmail.com>
2020-02-21 16:00:52 +0100

    risolto ancora code smell

--- Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/ProgettoAsteApplication.java
+++ Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/ProgettoAsteApplication.java
@@ -1,11 +1,7 @@
 package com.gruppoaste2.progettoaste;
 
-import com.gruppoaste2.progettoaste.model.PortafoglioModel;
-import com.gruppoaste2.progettoaste.model.UtenteRegistratoModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.UUID;
 
 @SpringBootApplication
 public class ProgettoAsteApplication {
--- Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/model/AmministratoreModel.java
+++ Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/model/AmministratoreModel.java
@@ -7,19 +7,4 @@ public class AmministratoreModel extends UtenteModel {
     public AmministratoreModel(UUID id, String username, String email) {
         super(id, username, email);
     }
-
-    @Override
-    public UUID getId() {
-        return super.getId();
-    }
-
-    @Override
-    public String getUsername() {
-        return super.getUsername();
-    }
-
-    @Override
-    public String getEmail() {
-        return super.getEmail();
-    }
 }
