9b90c153ca53d54784f4490510c13d965c32997b
wailla <f.villa71@campus.unimib.it>
2020-02-21 10:21:16 +0100

    aggiunta PostgresUtenteRegistrtoDAO

---
+++ Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/dao/UtenteRegistratoDAO.java
@@ -0,0 +1,19 @@
+package com.gruppoaste2.progettoaste.dao;
+
+import com.gruppoaste2.progettoaste.model.UtenteRegistratoModel;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UtenteRegistratoDAO {
+    public boolean insersciUtenteRegistrato(UtenteRegistratoModel utenteRegistrto);
+
+    public boolean eliminaUtenteRegistrato(UUID id);
+
+    public UtenteRegistratoModel trovaUtenteRegistro(UUID id);
+
+    public List<UtenteRegistratoModel> trovaTuttiUtentiRegistrati();
+
+    public boolean aggiornaUtenteRegistrto(UUID id, UtenteRegistratoModel utenteAggiornato);
+
+}
---
+++ Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/service/UtenteRegistrtoService.java
@@ -0,0 +1,4 @@
+package com.gruppoaste2.progettoaste.service;
+
+public class UtenteRegistrtoService {
+}
