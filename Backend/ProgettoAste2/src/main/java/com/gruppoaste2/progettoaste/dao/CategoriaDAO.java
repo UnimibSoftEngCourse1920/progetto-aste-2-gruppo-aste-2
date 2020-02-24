e30a68930c6394e94d956ef3fe6d52f3df73b085
wailla <f.villa71@campus.unimib.it>
2020-02-24 14:56:58 +0100

    Creazione categoria non terminata da correggere T

---
+++ Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/dao/CategoriaDAO.java
@@ -0,0 +1,19 @@
+package com.gruppoaste2.progettoaste.dao;
+
+import com.gruppoaste2.progettoaste.model.CategoriaModel;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CategoriaDAO {
+
+    public boolean insersciCategoria(CategoriaModel categtegoria);
+
+    public boolean eliminaCategoria(UUID id);
+
+    public CategoriaModel trovaCategoria(UUID id);
+
+    public List<CategoriaModel> trovaCategorie();
+
+    public boolean aggiornaCategoria (UUID id, CategoriaModel categoriAggiornata);
+}
