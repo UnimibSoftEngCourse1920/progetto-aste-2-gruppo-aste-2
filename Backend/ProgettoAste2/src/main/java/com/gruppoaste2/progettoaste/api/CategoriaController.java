e05ae8514b820656dbb3bdd148e4f4ed8f890203
wailla <f.villa71@campus.unimib.it>
2020-02-25 11:46:35 +0100

    Termine backend base

---
+++ Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/api/CategoriaController.java
@@ -0,0 +1,15 @@
+package com.gruppoaste2.progettoaste.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/categoria")
+@RestController
+public class CategoriaController {
+    @GetMapping
+    public String esempioGet()
+    {
+        return "<h1>Prova ciao hai fatto una get ad localhost:8080/api/asta<h1>";
+    }
+}
---
+++ Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/service/CategoriaService.java
@@ -0,0 +1,41 @@
+package com.gruppoaste2.progettoaste.service;
+
+import com.gruppoaste2.progettoaste.dao.CategoriaDAO;
+import com.gruppoaste2.progettoaste.model.CategoriaModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class CategoriaService {
+    private final CategoriaDAO categoriaDAO;
+
+    @Autowired
+    public CategoriaService(@Qualifier("postgres-categoria") CategoriaDAO categoriaDAO) {
+        this.categoriaDAO = categoriaDAO;
+    }
+
+    public boolean inserisciCategoria(CategoriaModel categoria){
+        return categoriaDAO.insersciCategoria(categoria);
+
+    }
+
+    public boolean eliminaCategoria(UUID id){
+        return categoriaDAO.eliminaCategoria(id);
+    }
+
+    public CategoriaModel trovaCategoria(UUID id){
+        return categoriaDAO.trovaCategoria(id);
+    }
+
+    public List<CategoriaModel> trovaCategorie(){
+        return categoriaDAO.trovaCategorie();
+    }
+
+    public boolean aggiornaCategoria (UUID id, CategoriaModel categoriAggiornata){
+        return categoriaDAO.aggiornaCategoria(id, categoriAggiornata);
+    }
+}
