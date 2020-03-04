e69a26fd0f47e50300963af244fd9b49fd030cc9
Francesco Frigati <f.frigati@campus.unimib.it>
2020-03-04 22:08:42 +0100

    Creati CategoriaControllerTest e OggettoControllerTest e rinominato le classi

---
+++ Backend/ProgettoAste2/src/test/java/com/gruppoaste2/progettoaste/api/CategoriaControllerTest.java
@@ -0,0 +1,121 @@
+package com.gruppoaste2.progettoaste.api;
+
+import com.gruppoaste2.progettoaste.model.CategoriaModel;
+import com.gruppoaste2.progettoaste.model.ConfigurazioneModel;
+import com.gruppoaste2.progettoaste.service.CategoriaService;
+import com.gruppoaste2.progettoaste.service.ConfigurazioneService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CategoriaController.class)
+class CategoriaControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CategoriaService categoriaService;
+
+    // Test trovaCategoria
+    @Test
+    public void whenTrovaCategoria_givenNonExistingCategoria_thenReturnEmptyJson() throws Exception {
+
+    }
+
+    @Test
+    public void whenTrovaCategoria_givenExistingCategoria_thenReturnJsonMapCategoria() throws Exception {
+
+    }
+
+    // Test trovaCategorie
+    @Test
+    public void whenTrovaCategorie_givenNonExistingCategorie_thenReturnEmptyJsonArray() throws Exception {
+
+    }
+
+    @Test
+    public void whenTrovaCategorie_givenExistingCategorie_thenReturnJsonArrayOfMapsCategorie() throws Exception {
+
+    }
+
+    // Test inserisciCategoria
+    @Test
+    public void whenInserisciAmministratore_givenExistingAmministratore_thenReturnJsonNumber0() throws Exception {
+
+    }
+
+    @Test
+    public void whenInserisciAmministratore_givenNonExistingAmministratore_thenReturnJsonNumber1() throws Exception {
+
+    }
+
+    // Test aggiornaCategoria
+    @Test
+    public void whenAggiornaAmministratore_givenNonExistingAmministratore_thenReturnJsonNumber0() throws Exception {
+
+    }
+
+    @Test
+    public void whenAggiornaAmministratore_givenExistingAmministratore_thenReturnJsonNumber1() throws Exception {
+
+    }
+
+    // Test eliminaCategoria
+    @Test
+    public void whenEliminaAmministratore_givenNonExistingAmministratore_thenReturnJsonNumber0() throws Exception {
+
+    }
+
+    @Test
+    public void whenEliminaAmministratore_givenExistingAmministratore_thenReturnJsonNumber1() throws Exception {
+
+    }
+
+    // Test trovaAttributiCategoria
+    @Test
+    public void whenTrovaAttributiCategoria_givenNonExistingAttributiCategoria_thenReturnEmptyJsonArray() throws Exception {
+
+    }
+
+    @Test
+    public void whenTrovaAttributiCategoria_givenExistingAttributiCategoria_thenReturnJsonArrayOfMapsAttributiCategoria() throws Exception {
+
+    }
+
+    // Test trovaCategorieOggetto
+    @Test
+    public void whenTrovaCategorieOggetto_givenNonExistingCategorieOggetto_thenReturnEmptyJsonArray() throws Exception {
+
+    }
+
+    @Test
+    public void whenTrovaCategorieOggetto_givenExistingCategorieOggetto_thenReturnJsonArrayOfMapsCategorieOggetto() throws Exception {
+
+    }
+
+    // Test valoreAttributiOggetto
+    @Test
+    void valoreAttributoOggetto() {
+    }
+}
---
+++ Backend/ProgettoAste2/src/test/java/com/gruppoaste2/progettoaste/api/OggettoControllerTest.java
@@ -0,0 +1,112 @@
+package com.gruppoaste2.progettoaste.api;
+
+import com.gruppoaste2.progettoaste.service.CategoriaService;
+import com.gruppoaste2.progettoaste.service.OggettoService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(OggettoController.class)
+class OggettoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OggettoService oggettoService;
+
+    // Test inserisciOggetto
+    @Test
+    public void whenInserisciOggetto_givenExistingOggetto_thenReturnJsonNumber0() throws Exception {
+
+    }
+
+    @Test
+    public void whenInserisciOggetto_givenNonExistingOggetto_thenReturnJsonNumber1() throws Exception {
+
+    }
+
+    // Test aggiornaOggetto
+    @Test
+    public void whenAggiornaOggetto_givenNonExistingOggetto_thenReturnJsonNumber0() throws Exception {
+
+    }
+
+    @Test
+    public void whenAggiornaOggetto_givenExistingOggetto_thenReturnJsonNumber1() throws Exception {
+
+    }
+
+    // Test trovaOggetto
+    @Test
+    public void whenTrovaOggetto_givenNonExistingOggetto_thenReturnEmptyJson() throws Exception {
+
+    }
+
+    @Test
+    public void whenTrovaOggetto_givenExistingOggetto_thenReturnJsonMapOggetto() throws Exception {
+
+    }
+
+    // Test trovaOggetti
+    @Test
+    public void whenTrovaOggetti_givenNonExistingOggetti_thenReturnEmptyJsonArray() throws Exception {
+
+    }
+
+    @Test
+    public void whenTrovaOggetti_givenExistingOggetti_thenReturnJsonArrayOfMapsOggetti() throws Exception {
+
+    }
+
+    // Test trovaOggettiAsta
+    @Test
+    public void whenTrovaOggettiAsta_givenNonExistingOggettiAsta_thenReturnEmptyJsonArray() throws Exception {
+
+    }
+
+    @Test
+    public void whenTrovaOggettiAsta_givenExistingOggettiAsta_thenReturnJsonArrayOfMapsOggettiAsta() throws Exception {
+
+    }
+
+    // Test oggettiRegistratiDaUtente
+    @Test
+    public void whenTrovaOggettiRegistratiDaUtente_givenNonExistingOggettiRegistratiDaUtente_thenReturnEmptyJsonArray() throws Exception {
+
+    }
+
+    @Test
+    public void whenTrovaOggettiRegistratiDaUtente_givenExistingOggettiRegistratiDaUtente_thenReturnJsonArrayOfMapsOggettiRegistratiDaUtente() throws Exception {
+
+    }
+
+    // Test oggettiInCorsoAstaUtente
+    @Test
+    public void whenTrovaOggettiInCorsoAstaUtente_givenNonExistingOggettiInCorsoAstaUtente_thenReturnEmptyJsonArray() throws Exception {
+
+    }
+
+    @Test
+    public void whenTrovaOggettiInCorsoAstaUtente_givenExistingOggettiInCorsoAstaUtente_thenReturnJsonArrayOfMapsOggettiInCorsoAstaUtente() throws Exception {
+
+    }
+
+    // Test oggettiVintiDaUtente
+    @Test
+    public void whenTrovaOggettiVintiDaUtente_givenNonExistingOggettiVintiDaUtente_thenReturnEmptyJsonArray() throws Exception {
+
+    }
+
+    @Test
+    public void whenTrovaOggettiVintiDaUtente_givenExistingOggettiVintiDaUtente_thenReturnJsonArrayOfMapsOggettiVintiDaUtente() throws Exception {
+
+    }
+}
