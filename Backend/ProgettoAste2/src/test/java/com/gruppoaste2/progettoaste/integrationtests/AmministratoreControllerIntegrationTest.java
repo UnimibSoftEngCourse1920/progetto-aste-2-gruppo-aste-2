232ce191022535c235dd854d04e7f448204551ed
Andrea Tassi <a.tassi1@campus.unimib.it>
2020-03-13 01:47:14 +0100

    Fixed configuration for IntegrationTests

--- Backend/ProgettoAste2/src/test/java/com/gruppoaste2/progettoaste/integrationtests/AmministratoreControllerIntegrationTest.java
+++ Backend/ProgettoAste2/src/test/java/com/gruppoaste2/progettoaste/integrationtests/AmministratoreControllerIntegrationTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "/resources/application.properties")
+// @TestPropertySource(locations = "resources/application.properties")
 class AmministratoreControllerIntegrationTest {
 
     @Autowired
@@ -43,7 +45,7 @@ class AmministratoreControllerIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$").doesNotExist());
-         */
+
     }
 
     @Test
