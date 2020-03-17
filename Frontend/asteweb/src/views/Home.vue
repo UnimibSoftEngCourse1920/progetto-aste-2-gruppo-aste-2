eb1eddcfdfed51a884b2c2113e4ec2ae3c6d41cb
wailla <f.villa71@campus.unimib.it>
2020-03-18 00:54:02 +0100

    sistemazione della pagie Home

--- Frontend/asteweb/src/views/Home.vue
+++ Frontend/asteweb/src/views/Home.vue
@@ -1,30 +1,42 @@
 <template>
-  <div class="home">
-    <div class="search-wrapper">
-      <label>Ricerca per nome:</label>
-      <input type="text" v-model="searchNome" placeholder="Search" />
-      <label>Ricerca per descrizione:</label>
-      <input type="text" v-model="searchDescrione" placeholder="Search" />
+  <div class="row">
+    <div class="col-lg-3">
+      <div class="search-wrapper">
+        <label>Ricerca per nome:</label>
+        <input type="text" v-model="searchNome" placeholder="Search" />
+        <p></p>
+        <label>Ricerca per descrizione:</label>
+        <input type="text" v-model="searchDescrione" placeholder="Search" />
+        <p></p>
+        <label>Ricerca per categoria:</label>
+        <ul v-for="categoria in categorie" :key="categoria.id">
+          
+        </ul>
+        <p>{{inputcb}}</p>
+      </div>
     </div>
-    <li v-for="asta in filteredList()" :key="asta.id">
-      <b-card :img-src="asta.oggetti[0].urlImmagine" img-alt="Card image" img-left class="mb-3">
-        <b-card-text>
-          <p class="text-left">nome: {{asta.oggetti[0].nome}}</p>
-          <div v-if="asta.infoAsta.tipo==='superamento_immediato'">
-            <div v-if="asta.prezzoPiuAlto &&asta.prezzoPiuAlto">
-              <p class="text-left">offerta corrente: {{asta.prezzoPiuAlto}}</p>
+    <div class="col-lg-9">
+      <h1>Aste</h1>
+      <li v-for="asta in filteredList()" :key="asta.id">
+        <b-card :img-src="asta.oggetti[0].urlImmagine" img-alt="Card image" img-left class="mb-3">
+          <b-card-text>
+            <p class="text-left">nome: {{asta.oggetti[0].nome}}</p>
+            <div v-if="asta.infoAsta.tipo==='superamento_immediato'">
+              <div v-if="asta.prezzoPiuAlto &&asta.prezzoPiuAlto">
+                <p class="text-left">offerta corrente: {{asta.prezzoPiuAlto}}</p>
+              </div>
+              <div v-else>
+                <p class="text-left">Prezzo: {{asta.infoAsta.prezzoPartenza}}</p>
+              </div>
             </div>
             <div v-else>
               <p class="text-left">Prezzo: {{asta.infoAsta.prezzoPartenza}}</p>
             </div>
-          </div>
-          <div v-else>
-            <p class="text-left">Prezzo: {{asta.infoAsta.prezzoPartenza}}</p>
-          </div>
-          <p class="text-left">{{asta.infoAsta.tipo}}</p>
-        </b-card-text>
-      </b-card>
-    </li>
+            <p class="text-left">{{asta.infoAsta.tipo}}</p>
+          </b-card-text>
+        </b-card>
+      </li>
+    </div>
   </div>
 </template>
 
@@ -36,20 +48,24 @@ export default {
     return {
       aste: [],
       searchNome: "",
-      searchDescrione:""
+      searchDescrione: "",
+      categorie: [],
+      inputcb: []
     };
   },
   methods: {
     filteredList() {
-        if (this.searchNome.length > 0 && this.searchDescrione==0)
-          return this.aste.filter(asta => {
-            return asta.oggetti[0].nome.includes(this.searchNome);
-          });
-         if (this.searchDescrione.length > 0 && this.searchNome==0)
-          return this.aste.filter(asta => {
-            return asta.oggetti[0].descrizione.includes(this.searchDescrione);
-          });
-         else return this.aste;
+      if (this.searchNome.length > 0 && this.searchDescrione == 0)
+        return this.aste.filter(asta => {
+          return asta.oggetti[0].nome.includes(this.searchNome.toLowerCase);
+        });
+      if (this.searchDescrione.length > 0 && this.searchNome == 0)
+        return this.aste.filter(asta => {
+          return asta.oggetti[0].descrizione.includes(
+            this.searchDescrione.toLowerCase
+          );
+        });
+      else return this.aste;
     },
     trovaOffertaMaggiore(asta) {
       let vm = this;
@@ -85,6 +101,14 @@ export default {
             vm.trovaOffertaMaggiore(asta);
           }
         });
+      });
+
+    fetch("http://localhost:8080/api/categoria/categorie", {
+      method: "get"
+    })
+      .then(response => response.json())
+      .then(response => {
+        this.categorie = response;
       });
   },
   computed: {}
