a0c3bd5d6e26766a0b6ea98643fd535cc372291d
AuPath <marco.au.grassi98@protonmail.com>
2020-03-18 11:26:02 +0100

    Aggiunta relazione, corretto errori ortografici in pagina help

--- Frontend/asteweb/src/views/Help.vue
+++ Frontend/asteweb/src/views/Help.vue
@@ -2,7 +2,7 @@
  <div class="text-center">
    <div class="text-justify">
    <p>Alcune piccole istruzioni per iniziare:</p>
-   <p> Se non sei ancora registrato, puoi farlo cloccando su sign in</p>
+   <p> Se non sei ancora registrato, puoi farlo cliccando su sign in</p>
    <p>Se invece sei già registrato puoi accedere cliccando la scritta login qui sopra</p>
    <p>Dopo il login puoi :</p>
    <li> Gestire il tuo credito acquistando e restituendo i punti credito e
@@ -10,11 +10,11 @@
    <li> Gestire il tuo portafoglio di oggetti registrati e vinti </li>
    <li> Creare un asta col pulsante crea asta: prima configura l'asta,
       poi inserisci il/gli oggetto/i che vuoi vendere sepecificandone gli attributi usufruendo di template per categorie.
-      In alternativa puoi anche importare/esportare liste di oggetti in formati standard (excel, csl, ...)</li>
+      </li>
    <li> visulizzare le aste in corso e parteciparvi. La modalita' dell'offerta dipende dal tipo di asta:</li> 
      <ul>- Busta chiusa, cioe' senza sapere quanto hanno puntato gli altri</ul>
-     <ul>- Superamnete immediato, cioe' le offerte sono pubbliche e ognuna deve superare la precedente</ul>
-   <li> In caso di vincita c'è la possibità di rinunciare agli oggetti acquistati ricevendo un risarcimento a meno di una penale
+     <ul>- Superamento immediato, cioe' le offerte sono pubbliche e ognuna deve superare la precedente</ul>
+   <li> In caso di vincita c'è la possibità di rinunciare all asta ricevendo un risarcimento a meno di una penale
      che può variare da asta ad asta </li>
    </div>
 </div>
