436adcba6811daff5ad266559e423171bed92d65
AuPath <marco.au.grassi98@protonmail.com>
2020-03-18 20:47:43 +0100

    Aggiornata relazione

--- Frontend/asteweb/src/views/AsteLoggedIn.vue
+++ Frontend/asteweb/src/views/AsteLoggedIn.vue
@@ -39,9 +39,12 @@
 
       <b-button>Visualizza oggetti</b-button>
 
-      <b-button v-if="selected === 'b' && asteDaVisualizzare[i - 1].infoAsta.rifiutata === false" v-on:click="rinuncia(i)">Rinuncia ad asta</b-button>
+      <b-button
+        v-if="selected === 'b' && asteDaVisualizzare[i - 1].infoAsta.rifiutata === false"
+        v-on:click="rinuncia(i)"
+      >Rinuncia ad asta</b-button>
 
-      <b-button v-if=" userId != asteDaVisualizzare[i - 1].astaManager.id && selected != 'b'">
+      <b-button v-if=" userId != asteDaVisualizzare[i - 1].astaManager.id && selected != 'b' && selected != 'e'">
         <router-link
           style="color:white"
           :to="{name: 'faiofferta', params: {idAsta: asteDaVisualizzare[i - 1].id}}"
@@ -63,7 +66,8 @@ export default {
         },
         { value: "b", text: "Visualizza aste vinte" },
         { value: "c", text: "Visualizza aste dove sei massimo offerente" },
-        { value: "d", text: "Visualizza aste a cui stai partecipando" }
+        { value: "d", text: "Visualizza aste a cui stai partecipando" },
+        { value: "e", text: "Visualizza aste che hai rifiutato" }
       ],
       asteDaVisualizzare: [],
       userId: null
@@ -149,6 +153,26 @@ export default {
       } else if (this.selected === "d") {
         fetch(
           "http://localhost:8080/api/asta/aste/incorso/offerente/" +
+            localStorage.getItem("idLog"),
+          { method: "get" }
+        )
+          .then(response => response.json())
+          .then(response => {
+            this.asteDaVisualizzare = response;
+          })
+          .then(() => {
+            var i;
+            for (i = 0; i < this.asteDaVisualizzare.length; i++) {
+              document
+                .getElementById(i + 1)
+                .getElementsByClassName(
+                  "card-img-left"
+                )[0].src = this.asteDaVisualizzare[i].oggetti[0].urlImmagine;
+            }
+          });
+      } else if (this.selected === "e") {
+        fetch(
+          "http://localhost:8080/api/asta/aste/rifiutate/" +
             localStorage.getItem("idLog"),
           { method: "get" }
         )
