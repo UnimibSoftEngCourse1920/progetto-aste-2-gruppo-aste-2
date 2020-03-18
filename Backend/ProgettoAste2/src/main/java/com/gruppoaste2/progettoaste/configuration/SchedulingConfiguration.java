7af7b7c088ce2672d38cf68dba596abfff8ff0ab
Andrea Tassi <a.tassi1@campus.unimib.it>
2020-03-18 20:58:40 +0100

    Fixed unused imports

--- Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/configuration/SchedulingConfiguration.java
+++ Backend/ProgettoAste2/src/main/java/com/gruppoaste2/progettoaste/configuration/SchedulingConfiguration.java
@@ -1,8 +1,6 @@
 package com.gruppoaste2.progettoaste.configuration;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
